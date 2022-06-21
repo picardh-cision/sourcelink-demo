7c7d02a44b52869b4df782d032dbadb9147a7a87
Hugo Picard <hugo.picard@cision.com>
2022-06-21 12:30:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# SourceLink.Demo
+
+Test application that uses the [SourceLink.Demo.Library](https://github.com/picardh-cision/sourcelink-demo-library) nuget package to test and showcase the SourceLink feature.
+
+To be able to compile and execute the application you will need to configure your local nuget client with a Github access token to the following nuget reposity : https://nuget.pkg.github.com/picardh-cision/index.json
+
+```powershell
+dotnet nuget add source --username [github username] --password [personal access token] --name PicardhCisionGithub https://nuget.pkg.github.com/picardh-cision/index.json
+```
+
---
+++ SourceLink.Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using SourceLink.Demo.Library.Extensions;
+using SourceLink.Demo.Library.Services;
+
+namespace SourceLink.Demo
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+	        var provider = BuildServiceProvider();
+	        var demoService = provider.GetRequiredService<IDemoService>();
+
+	        var result = demoService.GetDemoContent();
+
+	        Console.WriteLine(JsonSerializer.Serialize(result));
+        }
+
+		private static IServiceProvider BuildServiceProvider()
+			=> new ServiceCollection()
+				.SetupDemo(c => c.Client = "SourceLink.Demo.App")
+				.BuildServiceProvider();
+    }
+}
